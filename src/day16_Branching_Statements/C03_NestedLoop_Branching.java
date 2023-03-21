cb1a62bb26bba209369e7aadc7dd82c548574666
isikharun <isikharun@outlook.com>
2023-03-21 22:27:48 +0300

    day 17

--- src/day16_Branching_Statements/C03_NestedLoop_Branching.java
+++ src/day16_Branching_Statements/C03_NestedLoop_Branching.java
@@ -4,7 +4,7 @@ public class C03_NestedLoop_Branching {
 
 
     public static void main(String[] args) {
-
+/*
        outerLoop: for (int i = 0; i < 3; i++) {
             System.out.print("A");
            innerLoop: for (int j = 0; j <5 ; j++) {
@@ -20,10 +20,31 @@ public class C03_NestedLoop_Branching {
                }
 
         }
-
+*/
 //   Banka menüsü oluşturun 1 para ekle 2 hesabımı gör 3 para çek 4 çıkış  (money=2000)
         //  while işleme devam etmek için 1 e basın bir üst menü için 2 basın kart iade 3
 
+// 0 ile 100
+// dahil 30-50 arasını yazdırmadan
+// tüm sayıları ekrana yazın
+        for (int i = 0; i <=100 ; i++) {
+            if(i>30&&i<50){
+                continue;
+            }
+            System.out.print(i+" ");
+        }
+
+//// 0 ile 100
+//// dahil 13 bülünmeyen sayıları
+//// tüm sayıları ekrana yazın
+        for (int i = 0; i <=100 ; i++) {
+            if(i%13==0){
+                continue;
+            }
+            System.out.print(i+" ");
+        }
+
+    }
 
 
     }
@@ -32,4 +53,4 @@ public class C03_NestedLoop_Branching {
 
 
 
-}
+
