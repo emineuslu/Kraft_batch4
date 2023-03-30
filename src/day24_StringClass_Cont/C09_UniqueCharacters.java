dd244653c38453a5e835cd8c53a14804b13bbc17
ihsan <111647146+nested4@users.noreply.github.com>
2023-03-31 01:03:07 +0300

    day24 package added

--- src/day24_StringClass_Cont/C09_UniqueCharacters.java
+++ src/day24_StringClass_Cont/C09_UniqueCharacters.java
@@ -17,12 +17,8 @@ public class C09_UniqueCharacters {
 
 /*Write a program that can return the unique characters from a String
 
-			Ex:
-				input:
-					AABCCD
-
-				output:
-					BD
+			Ex:	input:AABCCD
+				output:BD
 			Hint: You will need indexOf() and lastIndexOf()
 				  if the first and last index numbers of the character are same, then it's unique
  */
