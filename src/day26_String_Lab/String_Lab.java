e8b08da552ad6768109b7fd141d9a25175e01a12
isikharun <isikharun@outlook.com>
2023-04-03 20:51:32 +0300

    day 26 commit 3

--- src/day26_String_Lab/String_Lab.java
+++ src/day26_String_Lab/String_Lab.java
@@ -4,9 +4,17 @@ public class String_Lab {
 
     public static void main(String[] args) {
 
+        String str="Harun"; // Har
+        // Ali Ali
+        // iş iş
+        // a  a
+        System.out.println(ilkUcHarfGetir(str))
+
 
         //Kullanıcıdan bir input alın ve girilen inputun ilk 3
         // harfini yazan , 3 kelimeden az ise inputu döndüren bir metot yazınız.
         //Merhaba Mer Ali Ali
     }
+
+
 }
