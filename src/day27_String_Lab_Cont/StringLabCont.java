a64201810154c7c3234e3bfd4be6ebaa872f73a4
isikharun <isikharun@outlook.com>
2023-04-04 21:35:13 +0300

    day 27 commit 3

--- src/day27_String_Lab_Cont/StringLabCont.java
+++ src/day27_String_Lab_Cont/StringLabCont.java
@@ -14,20 +14,18 @@ public class StringLabCont {
     Keskin ilçesinden geçiyorum false
      Neden kin tutuyorsun false */
 
-    public static void main(String[] args) {
 
-        String kelime="EKinlenmek";
-        String kontrol="kin";
-        System.out.println(ilkKelimeKontrol(kelime, kontrol));
-
-    }
 
     public static boolean ilkKelimeKontrol(String str,String target){
         if(target==null) return false;
         str=str.toLowerCase();
         target=target.toLowerCase();
      return str.startsWith(target)||str.substring(4).startsWith(target);
+    }
 
+    public static boolean ilkKelimeKontrol2(String str,String target){
+
+        return str.substring(0,target.length()+1).contains(target);
     }
 
 
@@ -39,5 +37,45 @@ public class StringLabCont {
     // MehmetMehmetMehmetMehmetMehmetMehmetMe hmetMehmetMehmetMehmetMehmetMehmetMehmet
     // MehmetMehmetMehmetMe hmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMe
     // hmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetMehmetAliAli
+
+
+
+    public static String cokluYazdir(String str1,String str2,int ilkDongu,int ortaDongu,int sonDongu){
+        String result="";
+        if(str1.length()<str2.length()){
+
+            for (int i = 0; i <ilkDongu ; i++) {
+                result+=str1;
+            }
+            //result=str1+str1+str2+str2
+            for (int i = 0; i < ortaDongu; i++) {
+                result+=str2;
+            }
+            for (int i = 0; i <sonDongu ; i++) {
+                result+=str1;
+            }
+        }else {
+            for (int i = 0; i <ilkDongu ; i++) {
+                result+=str2;
+            }
+            //result=str1+str1+str2+str2
+            for (int i = 0; i < ortaDongu; i++) {
+                result+=str1;
+            }
+            for (int i = 0; i <sonDongu ; i++) {
+                result+=str2;
+            }
+        }
+        return result;
+
+    }
+
+
+    //Kullanıcıdan bir kelime veya söz alın ve ilk 5
+    // karakterini yazdırın. 5 karakterin altında bir kelime girdiği taktirde
+    // 5 karaktere nokta ile tamamlayın.
+    //Sabun Sabun Merhaba Merha Ali Ali..
+    //Hey Hey..
+    //Pi Pi...
 
 }
