2657b8048869c83afb0c4817e0dcda2cde94fd81
isikharun <isikharun@outlook.com>
2023-04-10 22:28:01 +0300

    day 31 5

--- src/day31_Arrays_1/C02_Array_1.java
+++ src/day31_Arrays_1/C02_Array_1.java
@@ -17,10 +17,28 @@ public class C02_Array_1 {
         z[4]=107;
 
 
-        System.out.println(z[1]);
+       // System.out.println(z[1]);
+    String[] str =new String[3];
+    str[0]="Harun";
+    str[1]="Serkan";
+    str[2]="Emine";
+     //   System.out.println(str[1]);
 
 
 
+
+        String str2[]={"Harun","Serkan","Emine"};
+       str2[0]="Hatice";
+        System.out.println(str2[2]);
+        //String[] str2 =new String[3];
+       // str2[0]="Harun";
+       // str2[1]="Serkan";
+        // str2[2]="Emine";
+
+
+        //2 değer alan int türünde bir array oluşturun
+        // değer atayın sonra ve bu iki değeri
+        // toplayın ve ekrna yazdırın
 
     }
 
