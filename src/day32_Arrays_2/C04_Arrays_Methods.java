a617ac521d466737c18fd1880fc23edb427f5063
isikharun <isikharun@outlook.com>
2023-04-11 23:45:36 +0300

    day 32 4

--- src/day32_Arrays_2/C04_Arrays_Methods.java
+++ src/day32_Arrays_2/C04_Arrays_Methods.java
@@ -15,13 +15,123 @@ public class C04_Arrays_Methods {
         }
 */
 
-        int array1[]={25,26,36};
-        double array2[]={35,26,36};
-        System.out.println(arrayToplam(array2));
+        int array1[]={3,3,5};
+        int array2[]={35,3,36};
+       arrayYazdir(arrayBirlestir2(array1,array2));
 
 
     }
-    //İnt array ın elemanlarının toplamını ve çarpımını yeni bir array içinde döndüren bir method yazın.
+   // Verilen 2 arrayi birleştirip array döndüren bir method yazın.
+
+    public static int[] arrayBirlestir2(int[] arr1,int[] arr2){
+
+        /// array1[]={3,3,5}; array2[]={35,3,36};
+        int[] yeniArray=new int[arr1.length+arr2.length];  //yeniArray[]={3,3,5,0,0,0};
+        for (int i = 0,j=0; i  <yeniArray.length  ;i++) {    //           0 1 2 3 4 5
+            if(i<arr1.length) {
+                yeniArray[i]=arr1[i];
+
+            }  else {
+                yeniArray[i]=arr2[j];
+                //
+                j++;
+            }
+        }
+        return yeniArray;
+
+
+    }
+
+
+
+
+    public static int[] arrayBirlestir(int[] arr1,int[] arr2){
+        int[] yeniArray=new int[arr1.length+arr2.length];
+
+        for (int i = 0; i <arr1.length ; i++) {
+            yeniArray[i]=arr1[i];
+        }
+        int j=0;
+        for (int i = arr1.length; i <yeniArray.length ; i++) {
+            yeniArray[i]=arr2[j];
+            j++;
+        }
+        return yeniArray;
+
+    }
+
+
+
+
+
+
+
+    public static void arrayYazdir(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            if (i==arr.length-1){
+                System.out.println(arr[i]);
+            }else{
+                System.out.print(arr[i]+"-");
+            }
+
+        }
+    }
+    public static void arrayYazdir(double[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            if (i==arr.length-1){
+                System.out.println(arr[i]);
+            }else{
+                System.out.print(arr[i]+"-");
+            }
+        }
+    }
+    public static void arrayYazdir(String[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            if (i==arr.length-1){
+                System.out.println(arr[i]);
+            }else{
+                System.out.print(arr[i]+"-");
+            }
+        }
+    }
+    public static void arrayYazdir(char[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            if (i==arr.length-1){
+                System.out.println(arr[i]);
+            }else{
+                System.out.print(arr[i]+"-");
+            }
+        }
+    }
+    public static void arrayYazdir(boolean[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            if (i==arr.length-1){
+                System.out.println(arr[i]);
+            }else{
+                System.out.print(arr[i]+"-");
+            }
+        }
+    }
+    //İnt array ın elemanlarının toplamını ve çarpımını yeni
+    // bir array içinde döndüren bir method yazın.
+
+     public static int[] toplamVeCarpim(int[] arr){
+        int toplam=0;
+        int carpim=1;
+        int yeniArray[]=new int[2];
+
+         for (int i = 0; i <arr.length ; i++) {
+             toplam+=arr[i];
+             carpim*=arr[i];
+         }
+         yeniArray[0]=toplam;
+         yeniArray[1]=carpim;
+         return yeniArray;
+
+
+     }
+
+
 
 
 
